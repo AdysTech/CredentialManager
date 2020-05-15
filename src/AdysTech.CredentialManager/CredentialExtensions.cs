98d5a6af3afb550fe5d9dc0e27527bc2b4f0c335
Drew Noakes <git@drewnoakes.com>
2020-05-15 14:52:02 +1000

    Support multi-targeting via a single project (#41)
    Use single project to target .NET Framework & Core. SDK-style projects allow multi-targeting which makes this much simpler.
    
    The AsysTech.CredentialManager project will also pack a single package that targets multiple frameworks

---
+++ src/AdysTech.CredentialManager/CredentialExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace AdysTech.CredentialManager
+{
+    public static class CredentialExtensions
+    {
+        public static ICredential ToICredential(this NetworkCredential cred)
+        {
+            if (cred == null)
+            {
+                return null;
+            }
+
+            return new Credential(cred);
+        }
+    }
+}
--- tests/CredentialManager.Test/CredentialManagerTest.cs
+++ tests/CredentialManager.Test/CredentialManagerTest.cs
@@ -300,5 +300,89 @@ namespace CredentialManagerTest
             Assert.AreEqual("domain.com", domain);
         }
 
+        [TestMethod, TestCategory("AppVeyor")]
+        public void TestICredential_LongComment()
+        {
+
+            try
+            {
+                string test = "test";
+                var cred = (new NetworkCredential(test, test, test)).ToICredential();
+                cred.TargetName = "TestSystem_Attributes";
+                cred.Comment = new String('*', 257);
+                Assert.ThrowsException<ArgumentException>(() => cred.SaveCredential(), "SaveCredential didn't throw ArgumentException for larger than 256 byte Comment");
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Unexpected exception of type {0} caught: {1}",
+                            e.GetType(), e.Message);
+                return;
+            }
+        }
+
+        [TestMethod, TestCategory("AppVeyor")]
+        public void TestICredential_LongPassword()
+        {
+
+            try
+            {
+                string test = "test";
+                var cred = (new NetworkCredential(test, new String('*', 512), test)).ToICredential();
+                cred.TargetName = "TestSystem_Attributes";
+                Assert.ThrowsException<ArgumentException>(() => cred.SaveCredential(), "SaveCredential didn't throw ArgumentException for larger than 512 byte password");
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Unexpected exception of type {0} caught: {1}",
+                            e.GetType(), e.Message);
+                return;
+            }
+        }
+        [TestMethod, TestCategory("AppVeyor")]
+        public void TestICredential_AttributesNullValue()
+        {
+
+            try
+            {
+                string test = "test";
+                var cred = (new NetworkCredential(test, test, test)).ToICredential();
+                cred.TargetName = "TestSystem_Attributes";
+                cred.Attributes = new Dictionary<string, Object>();
+                cred.Attributes.Add("sampleAttribute", null);
+
+                Assert.ThrowsException<ArgumentNullException>(() => cred.SaveCredential(), "SaveCredential didn't throw ArgumentNullException for null valued Attribute");
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Unexpected exception of type {0} caught: {1}",
+                            e.GetType(), e.Message);
+                return;
+            }
+        }
+
+#if !NET45
+        [TestMethod, TestCategory("AppVeyor")]
+        public void TestICredential_AttributesLargeValue()
+        {
+
+            try
+            {
+                string test = "test";
+                var cred = (new NetworkCredential(test, test, test)).ToICredential();
+                cred.TargetName = "TestSystem_Attributes";
+                cred.Attributes = new Dictionary<string, Object>();
+                cred.Attributes.Add("sampleAttribute", ValueTuple.Create("RegularUser", DateTime.UtcNow));
+
+                Assert.ThrowsException<ArgumentException>(() => cred.SaveCredential(), "SaveCredential didn't throw ArgumentException for larger than 256 byte Attribute");
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Unexpected exception of type {0} caught: {1}",
+                            e.GetType(), e.Message);
+                return;
+            }
+        }
+#endif
+
     }
 }
